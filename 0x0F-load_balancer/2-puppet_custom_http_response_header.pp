8596ff56f99f470ae89de6616113efa6f82ef401
Dharrenz09 <dharrydharrenzerah@gmail.com>
2023-01-18 03:46:27 -0800

    Done

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,18 @@
+#puppet advance
+exec {'update:
+  command  => 'sudo apt-get update',
+  provider => shell,
+}
+-> package {'nginx':
+  ensure => present,
+}
+-> file_line {'header line':
+  ensure => '/etc/nginx/sites-available/default',
+  line   => location / {
+  add_header X-Served-By ${hostname};",
+  match  => '^\tlocation / {',
+}
+-> exec { 'restart service':
+  command  => 'sudo service nginx restart',
+  provider => shell,
+}
