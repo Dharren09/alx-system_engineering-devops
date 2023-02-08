f0491f44925f6e05ffa8a17ad6c7c0375da94ff8
Dharren Pius Makoha <107412228+Dharren09@users.noreply.github.com>
2023-02-09 00:12:24 +0300

    Done

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,37 +1,31 @@
 #!/usr/bin/python3
 
-"""Returns information about employee TODO list progress"""
+"""returns information about employee TODO list progress"""
 
 import requests
+from sys import argv
 
+if __name__ == "__main__":
+    """returns information about employee TODO list progress"""
+    if len(argv) > 1:
+        url = "https://jsonplaceholder.typicode.com/users"
+        user_id = int(argv[1])
 
-def get_employee_todo_progress(employee_id):
-    # URL for API endpoint
-    url = f"https://jsonplaceholder.typicode.com/todos?userid={employee_id}"
+        response = requests.get("{}/{}".format(url, user_id))
+        name = response.json().get("name")
 
-    # Get the response from the API
-    response = requests.get(url)
+        if name is not None:
+            tasks = requests.get("{}/{}/todos".format(url, user_id))
+            number_of_tasks = len(tasks.json())
 
-    # Check if response was successful
-    if response.status_code == 200:
+            completed_tasks = []
+            for task in tasks.json():
+                if task.get("completed") is True:
+                    completed_tasks.append(task.get("title"))
 
-        # Get the JSON data from the response
-        data = response.json()
+            number_of_complete = len(completed_tasks)
 
-        # Count the number of completed tasks
-        completed_tasks = len([task for task in data if task["completed"]])
-        total_tasks = len(data)
-
-        # Get the employee name
-        employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-        employee_response = requests.get(url)
-        employee_data = employee_response.json()
-        employee_name = employee_data["name"]
-
-        # Print the results
-        print(f"Employee {employee_name} is done with tasks({completed_tasks}/{total_tasks}):")
-        for task in data:
-            if task["completed"]:
-                print(f"\t {task['title']}")
-    else:
-        print("An error occurred while retrieving the data")
+            print("Employee {} is done with tasks({}/{}):".format(name,
+                  number_of_complete, number_of_tasks))
+            for task in completed_tasks:
+                print("\t {}".format(task))
