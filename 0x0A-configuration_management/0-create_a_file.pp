213d0ff86c3bbb6d72e191df6d4b8512a0386113
root <root@iamdharrenzug.localdomain>
2023-03-03 18:07:42 +0300

    Cleaning Errors

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-# Create a file in /tmp
+#Create a file in /tmp
 file {'/tmp/school':
-mode     => '0744',
-owner    => 'www-data',
-group    => 'www-data',
-contents => 'I love Puppet',
-path     => '/tmp/school',
+    ensure  => 'file',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
 }
